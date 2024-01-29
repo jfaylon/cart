09488bf23a92c5bf5643bfa39de1c1cb0939eddb
Juan Faylon <juan.f@saltmine.com>
2024-01-29 19:13:12 +0800

    fix: styling

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -37,26 +37,30 @@ const CartPage = ({ params }) => {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 space-y-20">
-      <Header />
-      <p className="text-xl font-bold">{CART_TITLE}</p>
-      {cart.length > 0 ? (
-        <div className="max-w-2xl w-full mt-16">
-          <div className="flex flex-col">
-            {cart.map((item) => (
-              <CartItem key={item.id} item={item} updateCart={updateCart} />
-            ))}
-            <div className="mt-4 self-end">
-              <p className="text-xl font-bold">
-                Total Price: ${calculateTotalPrice()}
-              </p>
+    <div className="flex flex-col h-screen">
+      <div className="bg-black shadow-md z-10 leading-10">
+        <Header />
+      </div>
+      <main className="flex min-h-screen flex-col items-center p-24 space-y-20">
+        <p className="text-xl font-bold">{CART_TITLE}</p>
+        {cart.length > 0 ? (
+          <div className="max-w-2xl w-full mt-16">
+            <div className="flex flex-col">
+              {cart.map((item) => (
+                <CartItem key={item.id} item={item} updateCart={updateCart} />
+              ))}
+              <div className="mt-4 self-end">
+                <p className="text-xl font-bold">
+                  Total Price: ${calculateTotalPrice()}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ) : (
-        <p>No Items in Cart</p>
-      )}
-    </main>
+        ) : (
+          <p>No Items in Cart</p>
+        )}
+      </main>
+    </div>
   );
 };
 
